0d1ecf2b6c4dd8922c3ba4045b1b5b800c7dc722
mahmoud_zahran <mah.zahran.777@gmail.com>
2022-06-01 15:24:36 +0200

    first commit

---
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import android.R
+import androidx.test.espresso.Espresso.onView
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import tools.fastlane.screengrab.Screengrab
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.myapplication", appContext.packageName)
+    }
+
+
+    @Test
+    fun testTakeScreenshot() {
+        Screengrab.screenshot("click")
+    }
+}
